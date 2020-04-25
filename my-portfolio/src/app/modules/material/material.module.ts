44830ddd61067ee7559837412ee4be618b8ea584
Sayed <eliaszamanonline@gmail.com>
2020-04-25 11:50:20 +0600

    toolbar added

---
+++ my-portfolio/src/app/modules/material/material.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatIconModule } from  '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports:[
+    MatToolbarModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ]
+})
+export class MaterialModule { }
