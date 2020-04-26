a393f61c2df17c53a0ce19ce4ab707f9cf567910
Sayed <sayeduzzamancuet@gmail.com>
2020-04-26 20:36:14 +0600

    animaion added

--- my-portfolio/src/app/app.component.ts
+++ my-portfolio/src/app/app.component.ts
@@ -12,7 +12,15 @@ import {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('fade',[
+      transition('void=>*',[
+        style({opacity: 0}),
+        animate(2000)
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'my-portfolio';
