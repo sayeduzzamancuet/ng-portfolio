543052dee9cbc342b914b56bb3484a02d49fdcea
Sayed <sayeduzzamancuet@gmail.com>
2020-04-28 21:30:12 +0600

    route fix

--- my-portfolio/src/app/app.module.ts
+++ my-portfolio/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,19 +7,22 @@ import { MaterialModule } from './modules/material/material.module';
 import { HomeComponent } from './components/home/home.component';
 import {Routes,RouterModule} from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
-import { ContactComponent } from './components/contact/contact.component'
+import { ContactComponent } from './components/contact/contact.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component'
 
 const appRoutes: Routes=[
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
+  {path: 'portfolio', component: PortfolioComponent}
 ]
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
