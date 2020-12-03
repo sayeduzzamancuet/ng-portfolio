c33cd10d155737c7c004058ab9eaafd9ff93099c
Sayed <sayeduzzamancuet@gmail.com>
2020-12-03 20:18:33 +0600

    ng update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,22 +8,15 @@ import { Usermessage } from 'src/app/classes/usermessage';
 
 export interface PeriodicElement {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  institute: string;
+  year: number;
+  score: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  { name: 'BSc', institute: 'CUET', year: 2018, score: '2.8'},
+  { name: 'HSC', institute: 'Govt. Ananda mohon college, Mymensingh', year: 2012, score: '5'},
+  { name: 'SSC', institute: 'KB Highschool, Mymensingh', year: 2012, score: '5'}
 ];
 
 @Component({
@@ -41,7 +34,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 export class HomeComponent implements OnInit {
  
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['name', 'institute', 'year','score'];
   dataSource = ELEMENT_DATA;
   constructor() { 
   }
